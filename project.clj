8919d9ed4e0ffcc58bee0f276d553e27d3ec5a31
Weathercold <weathercold.scr@gmail.com>
2022-07-01 23:07:31 -0400

    .

--- project.clj
+++ project.clj
@@ -2,7 +2,7 @@
   :description "Mindustry Clojure Mod Template"
   :url "http://github.com/Weathercold/ExampleClojureModTemplate"
   :license {:name "GPL-3.0-or-later"
-            :url "https://www.gnu.org/licenses/gpl-3.0.txt"}
+            :url  "https://www.gnu.org/licenses/gpl-3.0.txt"}
 
   :repositories [["jitpack" "https://www.jitpack.io"]]
   :dependencies [[org.clojure/clojure "1.11.1"]]
@@ -16,13 +16,16 @@
   :java-source-paths ["src/java"]
   :resource-paths ["assets"]
   ;; Do not set :jar-name as it will break jar resolution in tasks
-  :uberjar-name "example-%s-desktop.jar"
+  :uberjar-name ~(-> #"defproject (\w*)"
+                   (clojure.core/re-find (slurp "project.clj"))
+                   second
+                   (str "-%s-desktop.jar"))
 
   :min-lein-version "2.0.0"
   :javac-options ["-source" "8" "-target" "8" "-Xlint:-options"]
   :aliases {"uberdex" ["do" "uberjar," "dex"]
-            "build-run" ["do" "uberjar," "copy," "launch"]
-            "dex-run" ["do" "uberjar," "dex," "copy," "launch"]}
+            "run-jar" ["do" "uberjar," "copy," "launch"]
+            "run-dex" ["do" "uberjar," "dex," "copy," "launch"]}
 
   ;; Uncomment for AOT (default)
   :aot :all
