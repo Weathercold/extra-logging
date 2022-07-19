c4338c0a1a8b0db9a5ae50d58f65775867527b94
Weathercold <weathercold.scr@gmail.com>
2022-07-19 10:18:22 -0400

    Revert cumbersome artifact unzipping, fix mod on android?

--- tasks/leiningen/dex.clj
+++ tasks/leiningen/dex.clj
@@ -9,19 +9,21 @@
             [leiningen.core.project :as project]
             [leiningen.jar :as jar]))
 
-(defn- find-android-jar []
-  (if-let [plat-dir
-           (if-let [sdk-root (or (System/getenv "ANDROID_SDK_ROOT")
-                                 (System/getenv "ANDROID_HOME"))]
-             (->> (.listFiles (io/file sdk-root "platforms"))
-                  sort
-                  reverse
-                  (filter #(.exists (io/file % "android.jar")))
-                  first)
-             nil)]
-    (str (.getPath plat-dir) "/android.jar")
-    (main/abort "Could not find android.jar file, make sure that an Android "
-                "SDK platform is installed.")))
+(def android-jar
+  (delay
+    (if-let [plat-dir
+             (if-let [sdk-root (or (System/getenv "ANDROID_SDK_ROOT")
+                                   (System/getenv "ANDROID_HOME"))]
+               (->> (io/file sdk-root "platforms")
+                    .listFiles
+                    sort
+                    reverse
+                    (filter #(.exists (io/file % "android.jar")))
+                    first)
+               nil)]
+      (str (.getPath plat-dir) "/android.jar")
+      (main/abort "Could not find android.jar file, make sure that an Android"
+                  "SDK platform is installed."))))
 
 (defn dex
   "Converts Java bytecode to DEX bytecode using d8.
@@ -43,8 +45,12 @@
     ;; --min-api is 26 because d8 says that MethodHandle.invoke and
     ;; MethodHandle.invokeExact are only supported
     ;; starting with Android 8.
-    (apply shell/sh (flatten ["d8" "--min-api" "26" "--lib" (find-android-jar)
-                              classpath standalone-name]))
-    (io/copy (io/file standalone-name) (io/file out-name))
-    (shell/sh "jar" "-uf" out-name "classes.dex")
-    (io/delete-file "classes.dex" true)))
+    (apply shell/sh (flatten ["d8"
+                              "--min-api" "26"
+                              "--lib" @android-jar
+                              classpath
+                              "--output" out-name
+                              standalone-name]))
+    ;; (shell/sh "jar" "-uf" out-name "classes.dex")
+    ;; (io/delete-file "classes.dex" true)
+    ))
