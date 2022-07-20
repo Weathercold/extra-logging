992ec4f3c9a1aea77b8f71bb9713f1da12abace1
Weathercold <weathercold.scr@gmail.com>
2022-07-20 14:13:08 -0400

    .

--- src/clojure/logging/core.clj
+++ src/clojure/logging/core.clj
@@ -1,27 +1,9 @@
 (ns logging.core
   (:require [logging.util.lambdas :refer :all])
-  (:import (arc Core Events)
-           (arc.util Log)
-           (mindustry.game EventType$ClientLoadEvent)
-           (mindustry.ui.dialogs BaseDialog)))
+  (:import (arc.util Log)))
 
 (defn main []
-  (Log/info "Loaded ExampleClojureMod constructor.")
-  (Events/on
-    EventType$ClientLoadEvent
-    (cons1
-      (fn [_]
-        (let [dialog (BaseDialog. "frog")
-              cont (.cont dialog)]
-          (doto cont
-            (-> (.add "behold")
-                .row)
-            (-> (.image (.find Core/atlas "example-clojure-mod-frog"))
-                (.pad 20.)
-                .row)
-            (-> (.button "I see" (runnable #(.hide dialog)))
-                (.size 100., 50.)))
-          (.show dialog))))))
+  (Log/info "Loaded ExampleClojureMod constructor."))
 
 (defn load-content []
   (Log/info "Loading some example content."))
