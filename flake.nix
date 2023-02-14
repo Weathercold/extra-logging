0563dc989509cd64428004daf9f0c41e4af85963
Weathercold <weathercold.scr@gmail.com>
2023-02-14 18:11:38 -0500

    Enable manual workflow trigger, testing jdk 8

--- flake.nix
+++ flake.nix
@@ -24,12 +24,12 @@
       formatter.x86_64-linux = nixpkgs-fmt;
       devShells.x86_64-linux.default = mkShell {
         buildInputs = [
-          temurin-bin
+          temurin-bin-8
           leiningen
           android-sdk
         ];
         shellHook = ''
-          export JAVA_HOME=${temurin-bin} \
+          export JAVA_HOME=${temurin-bin-8} \
                  ANDROID_SDK_ROOT=${android-sdk}/share/android-sdk \
                  PATH=$JAVA_HOME/bin:$ANDROID_SDK_ROOT/build-tools/33.0.1:$PATH
           mkdir ~/Android 2>/dev/null
