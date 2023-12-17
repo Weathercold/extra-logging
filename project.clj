2400fb693f4b7b2d1e25d4d446750bee081bb38c
Weathercold <weathercold.scr@proton.me>
2023-12-17 17:54:11 -0500

    Bump version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject extra-logging "2.1.0"
+(defproject extra-logging "2.2.0"
   :description "Extra Logging"
   :url "http://github.com/Weathercold/extra-logging"
   :license {:name "GPL-3.0-or-later"
